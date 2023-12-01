316340fc269dcd9db2c48fd399b420f94a1454e6
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-01 20:40:14 +0000

    Day 1.1 : Solved

--- main.c
+++ main.c
@@ -1,16 +1,84 @@
-#define BUFFER_SIZE 1024
+#include <stdio.h>
+#include <fcntl.h>			// open()
+#include "get_next_line.h"
 
-int main(void)
+typedef struct s_calib_val
 {
-	char	*vault;
-	char	*read_buffer;
-	int		bytes_read;
-	int		fd;
+	int first_dig;
+	int last_dig;
+	int calib_val;
+}		t_calib_val;
+
+t_calib_val		get_calib_val(char *line);
+
+
+int		main(void)
+{
+	t_calib_val	*read_vals;
+	char		*line;
+	int			*vals_list;
+	int			n_lines;
+	int			sum;
+	int			fd;
+	int			i;
 
 	fd = open("calibration-doc.txt", O_RDONLY);
 	if (fd == -1)
+	{
+		perror("open() failed");
 		return 1;
-	read_buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (read_buffer == NULL)
-		return 1;	
+	}
+	// Count number of lines
+	n_lines = 0;
+	while (get_next_line(fd) != NULL)
+		++n_lines;	
+	close(fd);
+	fd = open("calibration-doc.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open() failed");
+		return 1;
+	}
+	// Extract Values
+	i = 0;
+	vals_list = malloc(sizeof(int) * n_lines);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		// printf("%s", line);
+		read_vals = malloc(sizeof(t_calib_val));
+		*read_vals = get_calib_val(line);
+		vals_list[i] = read_vals->calib_val;
+		free(line);
+		++i;
+	}
+	close(fd);
+	// Sum values
+	sum = 0;
+	i = -1;
+	while (vals_list[++i])
+		sum += vals_list[i];
+	printf("The sum of all calibration values is: \n");
+	printf("\t%d\n", sum);
+
+}
+
+t_calib_val		get_calib_val(char *line)
+{
+	t_calib_val	calib_val;
+
+	calib_val.first_dig = -1;
+	calib_val.last_dig = -1;
+	calib_val.calib_val = 0;
+	while (*line) 
+	{
+		if (*line >= '0' && *line <= '9')
+		{
+			if (calib_val.first_dig == -1)
+				calib_val.first_dig = *line - '0';
+			calib_val.last_dig = *line - '0';
+		}
+		++line;
+	}
+	calib_val.calib_val = (calib_val.first_dig * 10) + calib_val.last_dig;
+	return (calib_val);
 }
