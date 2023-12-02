16a3f93f92661b7f508678ee665410df7f03fb75
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-02 10:50:30 +0000

    Days folders Renamed to .0 .1

--- Day_1.0/.gdbinit
+++ Day_1.0/.gdbinit
@@ -10,26 +10,14 @@ end
 
 define gcv
 	display calib_val
-	display /d calib_val->first_dig
-	display /d calib_val->last_dig
-	display /d calib_val->calib_val
-
-end
-
-define strnum
-display line
-	display n_strs
-	display numstr[i]
-	display i
-end
-
-define strtoi
-	display str
 end
 
 ## Run at startup
 set trace-commands on
 set logging on
 fs cmd
-break 50
-run
+break 56
+# run
+start
+main
+
--- Day_1.1/main.c
+++ Day_1.1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>			// open()
+#include <string.h>			// strstr()
 #include "get_next_line.h"
 
 typedef struct s_calib_val
@@ -10,6 +11,8 @@ typedef struct s_calib_val
 }		t_calib_val;
 
 t_calib_val		get_calib_val(char *line);
+int				is_strnum(char *line);
+int				strtoi(char *str);
 
 
 int		main(void)
@@ -42,10 +45,13 @@ int		main(void)
 	// Extract Values
 	i = 0;
 	vals_list = malloc(sizeof(int) * n_lines);
+	if (!vals_list)
+		return 1;
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		// printf("%s", line);
 		read_vals = malloc(sizeof(t_calib_val));
+		if (!read_vals)
+			return 1;
 		*read_vals = get_calib_val(line);
 		vals_list[i] = read_vals->calib_val;
 		free(line);
@@ -62,6 +68,7 @@ int		main(void)
 
 }
 
+
 t_calib_val		get_calib_val(char *line)
 {
 	t_calib_val	calib_val;
@@ -77,8 +84,63 @@ t_calib_val		get_calib_val(char *line)
 				calib_val.first_dig = *line - '0';
 			calib_val.last_dig = *line - '0';
 		}
+		if ((*line >= 'a' && *line <= 'z') || (*line >= 'A' && *line <= 'Z'))
+		{
+			if (calib_val.first_dig == -1)
+				calib_val.first_dig = is_strnum(line);
+			calib_val.last_dig = is_strnum(line);
+		}
 		++line;
 	}
 	calib_val.calib_val = (calib_val.first_dig * 10) + calib_val.last_dig;
 	return (calib_val);
+}
+
+int	is_strnum(char *line)
+{
+	int		n_strs;
+	int		i;
+	char	*pos;
+	char	*numstr[] = {	
+		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 
+	};
+	
+	n_strs = sizeof(numstr) / sizeof(numstr[0]);
+	i = 0;
+	while (i < n_strs)
+	{
+		pos = line;
+		while ((pos = strstr(pos, numstr[i])) != NULL)
+		{
+			return (strtoi(numstr[i]));
+			++pos;
+		}
+		++i;
+	}
+	return -1;
+}
+
+int		strtoi(char *str)
+{
+	if(strcmp(str, "zero") == 0) 
+		return 0;
+    else if(strcmp(str, "one") == 0) 
+		return 1;
+    else if(strcmp(str, "two") == 0) 
+		return 2;
+    else if(strcmp(str, "three") == 0) 
+		return 3;
+    else if(strcmp(str, "four") == 0) 
+		return 4;
+    else if(strcmp(str, "five") == 0) 
+		return 5;
+    else if(strcmp(str, "six") == 0) 
+		return 6;
+    else if(strcmp(str, "seven") == 0) 
+		return 7;
+    else if(strcmp(str, "eight") == 0) 
+		return 8;
+    else if(strcmp(str, "nine") == 0) 
+		return 9;
+    else return -1;
 }
