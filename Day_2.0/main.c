4a14d25547216506e42594ebc5dde2856bf74404
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-02 16:45:03 +0000

    Day 2.0 : s_round & s_game defined

--- Day_2.0/main.c
+++ Day_2.0/main.c
@@ -3,6 +3,21 @@
 #include <string.h>			// strstr()
 #include "get_next_line.h"
 
+typedef struct s_round
+{
+	int		blue;
+	int		red;
+	int		green;
+	t_round	*next;
+}		t_round;
+
+typedef struct s_game
+{
+	int		id;
+	t_round	*rounds;
+}		t_game;
+
+
 int main(void)
 {
 	int		fd;
@@ -14,10 +29,12 @@ int main(void)
 	if (fd == -1)
 		return (1);
 	/* Get number of lines in the file */
+	n_games = 0;
 	while ((line = get_next_line(fd)) != NULL)
 		++n_games;
 	/* Close the file */
 	close(fd);
+
 	/* Open the file for Parsing */
 	fd = open("files/test.txt", O_RDONLY);
 	if (fd == -1)
