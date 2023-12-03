9de27e76df26a86b298ee19816b6c3be2aaf5e4e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-03 11:29:47 +0000

    Day 1.0 : created header file

---
+++ Day_1.0/day1.h
@@ -0,0 +1,17 @@
+#ifndef DAY1_H
+# define DAY1_H
+
+#include <stdio.h>
+#include <fcntl.h>			// open()
+#include "get_next_line.h"
+
+typedef struct s_calib_val
+{
+	int		first_dig;
+	int		last_dig;
+	int		calib_val;
+}	t_calib_val;
+
+t_calib_val		get_calib_val(char *line);
+
+#endif
--- Day_1.0/main.c
+++ Day_1.0/main.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
-#include <fcntl.h>			// open()
-#include "get_next_line.h"
-
-typedef struct s_calib_val
-{
-	int first_dig;
-	int last_dig;
-	int calib_val;
-}		t_calib_val;
-
-t_calib_val		get_calib_val(char *line);
-
+#include "day1.h"
 
 int		main(void)
 {
