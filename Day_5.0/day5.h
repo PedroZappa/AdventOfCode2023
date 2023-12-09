27630a4eb9c3234903c7627213fd4e97f11a945e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-09 12:35:41 +0000

    Day_5.0/day5.h : Edited

---
+++ Day_5.0/day5.h
@@ -0,0 +1,9 @@
+#ifndef DAY5_H
+# define DAY5_H
+
+# include "../libft/libft.h"
+
+# define SEP	"<====|===|===|===|====>\n"
+# define SEPLNG	"<====|===|===|===|===|===|===|===|===|====>\n"
+
+#endif
