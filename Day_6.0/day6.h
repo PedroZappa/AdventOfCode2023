774d9dee57d644e92bff90dfc0a9a9ead9bc5950
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-14 17:35:04 +0000

    Day_6.0 : Basic Files setup

---
+++ Day_6.0/day6.h
@@ -0,0 +1,11 @@
+#ifndef DAY6_H
+# define DAY6_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libft/libft/libft.h"
+
+# define SEP	"<====|===|===|===|====>\n"
+# define SEPLNG	"<====|===|===|===|===|===|===|===|===|====>\n"
+
+#endif
---
+++ Day_6.0/main.c
@@ -0,0 +1,9 @@
+#include "day6.h"
+#include <stdio.h>
+
+int		main(void)
+{
+	printf("Day 6.0\n");
+	printf(SEP);
+	return (0);	
+}
