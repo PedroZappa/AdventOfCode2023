375cada6e6b2bdccd1b7370a3e405ef23f0777b4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-15 10:32:35 +0000

    Day_7.0/main.c : Outlined 󰦂

--- Day_7.0/main.c
+++ Day_7.0/main.c
@@ -5,6 +5,14 @@ int		main(void)
 	printf("Day 7.0\n");
 	printf(SEPLNG);
 
+	/* Open File */
+
+	/* Parse Data */
+
+	/* Sort Hands by Strength */
+	
+	/* Calculate Winnings */
+
 	printf(SEPLNG);
 	return (0);
 }
